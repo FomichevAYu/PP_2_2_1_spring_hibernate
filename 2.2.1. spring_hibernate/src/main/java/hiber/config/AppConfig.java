fbcdf7bb31dc0687a66763a14a70c097e494d945
FomichevAYu <Mailforspoify@yandex.ru>
2023-04-05 17:43:31 +0300

    add DB config

--- 2.2.1. spring_hibernate/src/main/java/hiber/config/AppConfig.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/config/AppConfig.java
@@ -28,10 +28,10 @@ public class AppConfig {
    @Bean
    public DataSource getDataSource() {
       DriverManagerDataSource dataSource = new DriverManagerDataSource();
-      dataSource.setDriverClassName(env.getProperty("db.driver"));
-      dataSource.setUrl(env.getProperty("db.url"));
-      dataSource.setUsername(env.getProperty("db.username"));
-      dataSource.setPassword(env.getProperty("db.password"));
+      dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+      dataSource.setUrl("jdbc:mysql://localhost:3306/testbd");
+      dataSource.setUsername("root");
+      dataSource.setPassword("Aezakmi42@");
       return dataSource;
    }
 
